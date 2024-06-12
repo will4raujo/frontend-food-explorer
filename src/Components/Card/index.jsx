4db93325e1ea519597ae5b62f4233080f15a273e
willian <willian.araujo1994@gmail.com>
2024-06-12 16:59:43 -0300

    fix: card structure

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,21 +1,41 @@
 import { Container } from './styles';
 import { ButtonControl } from '../ButtonControl';
+import { Button } from '../Button';
 import { FiHeart } from 'react-icons/fi';
 // import { PiPencilSimpleLight } from 'react-icons/pi';
+import { useState, useEffect } from 'react';
 
-export function Card({ image, title, description, price, onClick }) {
+export function Card({ image, title, description, price, onClick, favorite = false }) {
+  const [isFavorite, setIsFavorite] = useState(favorite);
+
+  const handleFavorite = () => {
+    setIsFavorite(prev => !prev);
+  };
+
+  useEffect(() => {
+    setIsFavorite(favorite);
+  }, [favorite]);
+
 
   return (
-    <Container onClick={onClick}>
-      <FiHeart />
+    <Container favorite={isFavorite}>
+      <button className='fav-button' onClick={handleFavorite}>
+        <FiHeart />
+      </button>
       {/* <PiPencilSimpleLight /> */}
-      <img src={image} alt='Card' />
+      <div className='content-wrapper' onClick={onClick}>
 
-      <h3>{title + ' >'}</h3>
-      <p>{description}</p>
-      <span className='price'>{price}</span>
+        <img src={image} alt='Card' />
 
-      <ButtonControl />
+        <h3>{title + ' >'}</h3>
+        <p>{description}</p>
+        <span className='price'>{price}</span>
+      </div>
+      <div className='action-buttons'>
+        <ButtonControl />
+        <Button>Incluir</Button>
+      </div>
+
     </Container>
   );
 }
--- src/Components/Card/styles.js
+++ src/Components/Card/styles.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
 
   margin-right: 1.6rem;
-  
+  position: relative;
+
   display: flex;
   align-items: center;
   flex-direction: column;
   gap: 1.2rem;
 
-  position: relative;
   width: 21rem;
   height: 29.2rem;
 
@@ -17,49 +17,82 @@ export const Container = styled.div`
 
   border-radius: 8px;
   border: 1px solid ${({ theme }) => theme.COLORS.Dark_300};
+  
   background-color: ${({ theme }) => theme.COLORS.Dark_200};
 
   cursor: pointer;
 
-  > img {
-    width: 8.8rem;
-    height: 8.8rem;
-  }
-
-  > svg {
+  > .fav-button {
     position: absolute;
     top: 1.6rem;
     right: 1.6rem;
 
-    width: 2.4rem;
-    height: 2.2rem;
+    border: none;
+    background: transparent;
 
-    color: ${({ theme }) => theme.COLORS.Light_300};
+    cursor: pointer;
+    z-index: 2;
+
+    > svg {
+      color: ${({ theme }) => theme.COLORS.Light_300};
+      width: 2.4rem;
+      height: 2.2rem;
+       
+      > path {
+        fill: ${({ theme, favorite }) => favorite ? theme.COLORS.Light_300 : theme.COLORS.Dark_300};
+      }
+    }
   }
 
-  > h3 {
-    font-size: 1.4rem;
-    font-weight: 500;
-    font-family: ${({ theme }) => theme.FONTS.Poppins};
-    color: ${({ theme }) => theme.COLORS.Light_300};
-    
-    line-height: 24px;
+  > .content-wrapper {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1.2rem;
+
+    > img {
+    width: 8.8rem;
+    height: 8.8rem;
+    }
+  
+    > h3 {
+      font-size: 1.4rem;
+      font-weight: 500;
+      font-family: ${({ theme }) => theme.FONTS.Poppins};
+      color: ${({ theme }) => theme.COLORS.Light_300};
+
+      line-height: 24px;
+    }
+
+    p {    
+      display: none;
+    }
+    .price {
+      
+      font-size: 1.6rem;
+      line-height: 100%;
+      color: ${({ theme }) => theme.COLORS.Cake_200};
+    } 
+  
+    > span {
+      font-size: 1.6rem;
+      line-height: 100%;
+      color: ${({ theme }) => theme.COLORS.Light_300};
+    }
   }
 
-  p {
-    display: none;
-  }
+  > .action-buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    justify-content: center;
+    gap: 1.6rem;
+    width: 100%;
 
-  .price {
-    font-size: 1.6rem;
-    line-height: 100%;
-    color: ${({ theme }) => theme.COLORS.Cake_200};
-  }
-
-  > span {
-    font-size: 1.6rem;
-    line-height: 100%;
-    color: ${({ theme }) => theme.COLORS.Light_300};
+    > button:nth-child(2) {
+      width: 100%;
+      height: 3.2rem;
+    }
   }
 
   @media (min-width: 768px) {
@@ -67,26 +100,38 @@ export const Container = styled.div`
     height: 46.2rem;
     width: 30.4rem;
 
-    > img {
-      width: 17.6rem;
-      height: 17.6rem;
+    > .content-wrapper {
+      > img {
+        width: 17.6rem;
+        height: 17.6rem;
+      }
+
+      > h3 {
+        font-size: 2.4rem;
+        line-height: 140%;
+      }
+
+      > p {
+        display: block;
+        font-size: 1.4rem;
+        font-weight: 400;
+        line-height: 160%;
+        color: ${({ theme }) => theme.COLORS.Light_400};
+      }
+
+      > .price {
+        font-size: 3.2rem;
+      }
     }
 
-    > h3 {
-      font-size: 2.4rem;
-      line-height: 140%;
-    }
+    > .action-buttons {
+      flex-direction: row;
 
-    p {
-      display: block;
-      font-size: 1.4rem;
-      font-weight: 400;
-      line-height: 160%;
-      color: ${({ theme }) => theme.COLORS.Light_400};
-    }
-
-    .price {
-      font-size: 3.2rem;
+      > button:nth-child(2) {
+        width: auto;
+        height: 4.8rem;
+        padding: 1.2rem 2.4rem;
+      }
     }
   }
  ` 

0f83d10042fff58cd6349b29aa111b3cae96a812
willian <willian.araujo1994@gmail.com>
2024-07-05 10:45:09 -0300

    feat: signIn workflow
    - new hook auth.js
    - new function signIn
    - envolved routes in AuthProvider
    - validate user ins index routes
    - integrated form in signIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,17 +3,34 @@ import { Button } from "../../Components/Button";
 import { Input } from "../../Components/Input";
 import { Logo } from "../../Components/Logo";
 import { ButtonText } from "../../Components/ButtonText";
+import { useState } from "react";
+import { useAuth } from '../../hooks/auth';
+
 
 export function SignIn() {
     
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const { signIn } = useAuth()
+
+
+    const handleSignIn = async (e) => {
+        e.preventDefault()
+        if (!email || !password) {
+            return alert('Preencha todos os campos')
+        }
+
+        signIn({ email, password })
+    }
+
     return (
         <Container>
                 <Logo/>
                 <Form>
                     <h1 className="form-title">Faça login</h1>
-                    <Input text={'Email'} type={'email'} placeholder='Exemplo: exemplo@exemplo.com.br'/>
-                    <Input text={'Senha'} type={'password'} placeholder='No mínimo 6 caracteres'/>
-                    <Button title={"Entrar"}></Button>
+                    <Input text={'Email'} type={'email'} placeholder='Exemplo: exemplo@exemplo.com.br' onChange={(e) => setEmail(e.target.value)}/>
+                    <Input text={'Senha'} type={'password'} placeholder='No mínimo 6 caracteres' onChange={(e) => setPassword(e.target.value)}/>
+                    <Button title={"Entrar"} onClick={handleSignIn}/>
 
                     <ButtonText to="/register" >Criar conta</ButtonText>
                 </Form>

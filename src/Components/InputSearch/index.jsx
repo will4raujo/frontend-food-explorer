6d94c0c66e0641c172d3d9ccd7ef7198bf38619b
willian <willian.araujo1994@gmail.com>
2024-07-09 16:49:23 -0300

    feat: refactoring inputsearch to present results
    - new list with products from query search

--- src/Components/InputSearch/index.jsx
+++ src/Components/InputSearch/index.jsx
@@ -1,11 +1,44 @@
-import { Container } from './styles';
+import { Container, SearchResults } from './styles';
 import { FiSearch } from 'react-icons/fi';
+import { useState, useEffect } from 'react';
+import api from '../../services/api';
 
 export function InputSearch() {
-    return (
-        <Container>
-          <FiSearch />
-          <input type="text" placeholder="Pesquisar pratos" />
-      </Container>
-    )
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    if (search === '') {
+      api.get(`/dishes`).then(response => {
+        setResults(response.data);
+      });
+    }
+
+    if (search.length > 3) {
+      api.get(`/dishes/search?q=${search}`).then(response => {
+        setResults(response.data);
+      });
+    }
+  }, [search]);
+
+  return (
+    <Container>
+      <FiSearch />
+      <input 
+        type="text" 
+        placeholder="Busque por pratos ou ingredientes" 
+        value={search} 
+        onChange={(e) => setSearch(e.target.value)} 
+      />
+      {search && (
+        <SearchResults>
+          <ul>
+            {results.map(result => (
+              <li key={result.id}>{result.name}</li>
+            ))}
+          </ul>
+        </SearchResults>
+      )}
+    </Container>
+  );
 }

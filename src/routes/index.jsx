b805d6b059ef12d243b32c93d12d932a9b55839e
willian <willian.araujo1994@gmail.com>
2024-07-12 20:07:02 -0300

    feat: using token in cookie
    - removed all token configurations to local storage
    - ajusted logout to return a response status
    - added new prop in api, withCredentials

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,9 +2,20 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 import { useAuth } from "../hooks/auth";
+import { useEffect } from "react";
+import api from "../services/api";
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get('/users/validated')
+      .catch(error => {
+        if (error.response.status === 401) {
+          signOut();
+        }
+      });
+  }, []);
 
   return (
     <BrowserRouter>

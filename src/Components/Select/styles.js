a6a540a26c53b4cc0824fd03fa4b7181db106539
willian <willian.araujo1994@gmail.com>
2024-07-12 13:34:16 -0300

    feat: new prop semaphore in select component

--- src/Components/Select/styles.js
+++ src/Components/Select/styles.js
@@ -5,6 +5,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: .8rem;
+  position: relative;
 
   label {
     font-size: 1.6rem;
@@ -18,7 +19,9 @@ export const Container = styled.div`
     border-radius: 5px;
     border: none;
 
-    padding: 1.3rem 1.6rem;
+    padding-top: 1.3rem;
+    padding-bottom: 1.3rem;
+    padding-left: ${props => props.$semaphore ? '3.2rem' : '1.6rem'};
     font-size: 1.6rem;
 
     background-color: ${({ theme }) => theme.COLORS.Dark_900};
@@ -39,6 +42,23 @@ export const Container = styled.div`
       outline: none;
       box-shadow: 0 0 0 1px ${({ theme }) => theme.COLORS.Light_500};
     }
-    
+
   }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 1.6rem;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: ${({ theme, $semaphore }) => {
+      if ($semaphore === 'pending') return theme.COLORS.Tomato_300;
+      if ($semaphore === 'confirmed') return theme.COLORS.Carrot_100;
+      if ($semaphore === 'delivered') return theme.COLORS.Mint_100;
+    }};
+    z-index: 10;
+  }
+  
 `

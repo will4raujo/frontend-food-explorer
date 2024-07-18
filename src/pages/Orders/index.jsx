f6c845f245abeda93f074d1d61e44847c8a6e2c5
willian <willian.araujo1994@gmail.com>
2024-07-18 16:59:48 -0300

    fix: orders history to user

--- src/pages/Orders/index.jsx
+++ src/pages/Orders/index.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Card } from './styles';
+import { Container, Card, Table, Tr} from './styles';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { Select } from '../../components/Select';
 import api from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 
 export function Orders() {
   const [orders, setOrders] = useState([]);
   const [isMobile, setIsMobile] = useState(true);
+  const { user } = useAuth();
 
   const options = [
     { value: 'pending', label: 'Pendente' },
@@ -64,67 +66,138 @@ export function Orders() {
     <Container>
       <Header />
       <main>
-        <h1>Pedidos</h1>
-        {isMobile && (
+        { user.role === 'customer' && (
           <>
-            {orders.map((order, index) => (
-              <Card key={index}>
-                <div>
-                  <p>{`${order.id} - ${order.created_at}`}</p>
-                </div>
-                <p>{order.detailing}</p>
-                <Select
-                  name="status"
-                  title=""
-                  value={order.status}
-                  setValue={(value) => handleStatusChange(index, value)}
-                  options={options}
-                  semaphore={order.status}
-                />
-              </Card>
-            ))}
+            <h1>Histórico de pedidos</h1>
+            {isMobile && (
+              <>
+                {orders.map((order, index) => (
+                  <Card key={index} $semaphore={order.status}>
+                    <div>
+                        <div className='mobile-row-one'>
+                          <div>{order.id}</div> 
+                          <div>
+                            {order.status === 'pending' && 'Pendente'}
+                            {order.status === 'preparing' && 'Praparando'}
+                            {order.status === 'finished' && 'Finalizado'}
+                          </div>
+                          <div>{order.created_at}</div>
+                        </div>
+                    </div>
+                    <p>{order.detailing}</p>                   
+                  </Card>
+                ))}
+              </>
+            )}
+            { !isMobile && (
+                <> 
+                {orders.length > 0 ? (
+                  <Table >
+                    <thead>
+                      <tr>
+                        <th>Status</th>
+                        <th>Código</th>
+                        <th>Detalhamento</th>
+                        <th>Data e hora</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders.map((order, index) => (
+                        <Tr key={index} $semaphore={order.status}>
+                          <td>
+                            <div className='table-status'>
+                              {order.status === 'pending' && 'Pendente'}
+                              {order.status === 'preparing' && 'Praparando'}
+                              {order.status === 'finished' && 'Finalizado'}
+                            </div>
+                          </td>
+                          <td>{order.id}</td>
+                          <td>
+                            {order.detailing}
+                          </td>
+                          <td>{order.created_at}</td>
+                        </Tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                ) :
+                  <h1> Não há pedidos</h1>
+                }
+                </>
+            )}
           </>
         )}
-        { !isMobile && (
-            <> 
-            {orders.length > 0 ? (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Status</th>
-                    <th>Código</th>
-                    <th>Detalhamento</th>
-                    <th>Data e hora</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((order, index) => (
-                    <tr key={index}>
-                      <td>
-                        <Select
-                          name="status"
-                          title=""
-                          value={order.status}
-                          setValue={(value) => handleStatusChange(index, value)}
-                          options={options}
-                          semaphore={order.status}
-                        />
-                      </td>
-                      <td>{order.id}</td>
-                      <td>
-                        {order.detailing}
-                      </td>
-                      <td>{order.created_at}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            ) :
-              <h1> Não há pedidos</h1>
-            }
-            </>
-          )
-        }
+        { user.role === 'admin' && (
+          <>
+            <h1>Pedidos</h1>
+            {isMobile && (
+              <>
+                {orders.map((order, index) => (
+                  <Card key={index}>
+                    <div>
+                      { user.role === 'customer' &&
+                        <div className='mobile-row-one'>
+                          <div>{order.id}</div> 
+                          <div>{order.status}</div>
+                          <div>{order.created_at}</div>
+                        </div>
+                      }
+                    </div>
+                    <p>{order.detailing}</p>
+                    {user.role === 'admin' && <Select
+                      name="status"
+                      title=""
+                      value={order.status}
+                      setValue={(value) => handleStatusChange(index, value)}
+                      options={options}
+                      semaphore={order.status}
+                    />}
+                    
+                  </Card>
+                ))}
+              </>
+            )}
+            { !isMobile && (
+                <> 
+                {orders.length > 0 ? (
+                  <Table>
+                    <thead>
+                      <tr>
+                        <th>Status</th>
+                        <th>Código</th>
+                        <th>Detalhamento</th>
+                        <th>Data e hora</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders.map((order, index) => (
+                        <Tr key={index}>
+                          <td>
+                            <Select
+                              name="status"
+                              title=""
+                              value={order.status}
+                              setValue={(value) => handleStatusChange(index, value)}
+                              options={options}
+                              semaphore={order.status}
+                            />
+                          </td>
+                          <td>{order.id}</td>
+                          <td>
+                            {order.detailing}
+                          </td>
+                          <td>{order.created_at}</td>
+                        </Tr>
+                      ))}
+                    </tbody>
+                  </Table>
+                ) :
+                  <h1> Não há pedidos</h1>
+                }
+                </>
+            )}
+          </>
+        )}
       </main>
       <Footer />
     </Container>
